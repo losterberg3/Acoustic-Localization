90468bff239b49e97a17b13a37d8011901529a8e
Lars Osterberg <larsosterberg@Lars-MacBook-Air.local>
2024-07-18 12:55:24 -0700

    new sampling rate
    masterarduino and crosscorrelation codes reflect the new sampling rate

--- Matlab Code/crosscorrelation.m
+++ Matlab Code/crosscorrelation.m
@@ -1,39 +1,39 @@
 %% connect to serial port, only needs to be done once after the arduino is programmed
 
-s=serialport('/dev/cu.usbmodem141101',115200)  %check command window, make sure baud rate is correct
+s=serialport('/dev/cu.usbmodem141101',2000000)  %check command window, make sure baud rate is correct
 %% run this section once reset button on arduino has been pressed
 
 flush(s)    %clear the serial port receiving the data
-q=read(s,8927,'uint8');  %read the data, 8927 samples for a sampling duration of 1 second
+q=read(s,43194,'uint8');  %read the data, 43194 samples for a sampling duration of 1 second
 p=q-127; %using the DC offset to bring the samples around 0 and then to normalize the samples
 p=p./127; %originally the DC offset was 507, but we had to convert to an 8 bit number to maximize the serial baud rate
+sr=43194; %sampling rate
 
 cl=0.5; %chirplength in seconds
 sf=100; %start frequency of the chirp
 ef=1000; %end frequency of the chirp
-t=0:1/8926:1; %time for the chirp
+t=0:1/(sr-1):1; %time we are listening for the chirp
 f=(t.^2*(ef-sf)/cl/2)+(t.*sf); %frequency linearly increasing (frequency is the derivative of this wrt t)
 ref=sin(f.*2*pi); %reference chirp signal
-ref((cl*8926):8927)=0; %setting the rest of the reference signal to 0
-
+ref((cl*43194):sr)=0; %setting the rest of the reference signal to 0, because our t is a total of 1 sec, but our chirp is only 0.5 sec
 X=fft(ref); %fft of the reference signal
 Y=fft(p); %fft of the sampled signal
 Z=conj(X).*Y; %multiplying them in frequency domain
 z=ifft(Z); %ifft of the result to get back to the time domain
 [m,i]=max(z); %finding the index of the peak
-tof=i*(1/8926); %converting that index to a time
+tof=i*(1/(sr-1)); %converting that index to a time
 d=tof*343; %converting the tof to a distance by multiplying by the speed of sound
 
-%% 
-tt=0:1/8926:99/8926;
+%%
+tt=0:1/(sr-1):199/(sr-1);
 l=zeros(1,1000);
-l(:,1:100)=sin(500*2*pi*tt);
+l(:,1:200)=sin(1500*2*pi*tt);
 figure
 plot(l)
 title('ref')
 
 w=zeros(1,1000);
-w(:,201:300)=p(:,101:200);
+w(:,201:400)=p(:,101:300);
 figure
 plot(w);
 title('sampled')
@@ -44,6 +44,5 @@ Z=Y.*conj(X); %always use the conjugate of the reference signal fft
 z=ifft(Z);
 figure
 plot(z)
-%% 
-plot(p(:,1:1000))
+
 
--- masterarduino/masterarduino.ino
+++ masterarduino/masterarduino.ino
@@ -1,16 +1,22 @@
-int val=0;
-
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(115200); //check baud rate
+Serial.begin(2000000); //check baud rate
+ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+//ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8, 153846 hz theoretically
+ADCSRA |= bit (ADPS2);                               //  16, 76923 hz theoretically
+//ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32, 38462 hz theoretically
+//ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64, 19231 hz theoretically
+//ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128, 9615 hz theoretically
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 delay(3000); //allow enough time to click run on matlab
-while (millis()<=4000){ //sampling for 1 second
-  val=analogRead(0)-380; //converting analog value to an 8-bit integer
-  Serial.write(val); //writing the sampled value to the port
+//Serial.println(micros()); //I found that it actually starts sampling at 3.000016 seconds.
+while (micros()<=4000016){ //sampling for 1 second
+  Serial.write(analogRead(0)/4); //writing the sampled value to the port, dividing by 4 to make an 8 bit value
 }
 delay(60000);
 }
+
+
